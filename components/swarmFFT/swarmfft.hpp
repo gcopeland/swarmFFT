0251d3a8a7f270ac2eb4c293db69247d825e7eae
Greg Copeland <greg@copelandconsulting.net>
2026-05-24 19:29:45 -0500

    Increase sampling rate
    Testing

--- components/swarmFFT/swarmfft.hpp
+++ components/swarmFFT/swarmfft.hpp
@@ -43,11 +43,11 @@ namespace swarm_fft_audio {
     static const uint16_t CHANNELS = 2;
     static const uint16_t BITS_PER_SAMPLE = 32;
     static const uint16_t SAMPLE_LENGTH = 512;
-    static const uint16_t MAX_FREQUENCY_HZ = 2000;
-    static const uint16_t SAMPLES_PER_SECOND = MAX_FREQUENCY_HZ;
+    static const uint16_t MAX_FREQUENCY_HZ = 1200;
+    static const uint16_t SAMPLES_PER_SECOND = MAX_FREQUENCY_HZ * 3;
     static const uint16_t FFT_BINS = SAMPLE_LENGTH / 2;
     static const uint16_t MIN_FREQ_THRESHOLD = 40;
-    static const uint16_t MAX_FREQ_THRESHOLD = 1200;
+    static const uint16_t MAX_FREQ_THRESHOLD = MAX_FREQUENCY_HZ;
 
 
 
